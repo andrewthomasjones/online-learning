3bc0f40eb67228ff914bb3d72afbfbb926cb14c2
Andrew Jones <andrewthomasjones@gmail.com>
2020-04-30 17:20:56 +1000

    simple python script to generate a toy datastream

---
+++ svm-beam-online/data_source.py
@@ -0,0 +1,58 @@
+#pip install --upgrade google-cloud-pubsub
+#export GOOGLE_APPLICATION_CREDENTIALS="/Users/andrewjones/Projects/svm-beam-online/sandbox-vm-252402-d51aad228cb8.json"
+"""Publishes multiple messages to a Pub/Sub topic with an error handler."""
+import time
+import datetime
+import json
+from google.cloud import pubsub_v1
+import numpy as np
+
+project_id = "sandbox-vm-252402"
+topic_name = "svm-data-stream"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_name)
+
+futures = dict()
+
+def generateSim():
+  DELTA = 2
+  split = 0.5
+  class_names = ["A", "B"]
+
+  cov1 = [[1, 0], [0, 1]]
+  cov2 = [[1, 0], [0, 1]]
+
+  mean1 = [DELTA, DELTA]
+  mean2 = [-DELTA, -DELTA]
+
+  if(np.random.random()<split):
+      x, y = np.random.multivariate_normal(mean1, cov1, 1).T
+      c = class_names[0]
+  else:
+      x, y = np.random.multivariate_normal(mean2, cov2, 1).T
+      c = class_names[1]
+
+  payload = {
+        "x": float(x),
+        "y": float(y),
+        "class": str(c)
+        }
+
+  return payload
+
+while(True):
+    payload = generateSim()
+    data =  json.dumps(payload)
+    print(data)
+    futures.update({data: None})
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(
+        topic_path,
+        data=data.encode("utf-8"),
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        )
+    # Publish failures shall be handled in the callback function.
+    print(future.result())
+    # Wait for all the publish futures to resolve before exiting.
+    time.sleep(1)
